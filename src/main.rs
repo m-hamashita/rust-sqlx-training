8468eca33c48ba673a9ad1a57ab48b8de29fecce
m-hamashita <masakatsu.hamashita@gunosy.com>
2023-07-25 14:16:26 +0900

    Set transaction isolation level

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::mysql::MySqlPoolOptions;
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    let pool = MySqlPoolOptions::new()
+        .connect("mysql://user:password@localhost/test_db")
+        .await?;
+
+    // test query
+    // let row: (i64,) = sqlx::query_as("SELECT 1")
+    //     .fetch_one(&pool)
+    //     .await?;
+
+    // set transaction isolation level 
+    let _  = sqlx::query("set transaction isolation level read committed")
+        .execute(&pool)
+        .await?;
+
+    let row: (String, String) = sqlx::query_as("show variables like 'transaction_isolation'")
+        .fetch_one(&pool)
+        .await?;
+    println!("{} = {}", row.0, row.1);
+        
+
+    Ok(())
 }
